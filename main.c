69a0a2f84ebe8508076376b3ccecee937ae84677
Achmad Fadhitya Muharram <achmad.muharram@binus.ac.id>
2021-01-09 11:57:29 +0700

    Delete stack things

--- main.c
+++ main.c
@@ -2,125 +2,12 @@
 #include <stdlib.h>
 #define MAXN 7
 
-// STACK FOR HOLD CHARS
-
-// A structure to represent a stack
-struct Stack {
-    int top;
-    unsigned capacity;
-    char* array;
-};
- 
-// function to create a stack of given capacity. It initializes size of
-// stack as 0
-struct Stack* createStack(unsigned capacity)
-{
-    struct Stack* stack = (struct Stack*)malloc(sizeof(struct Stack));
-    stack->capacity = capacity;
-    stack->top = -1;
-    stack->array = (char*)malloc(stack->capacity * sizeof(char));
-    return stack;
-}
- 
-// Stack is full when top is equal to the last index
-int isFull(struct Stack* stack)
-{
-    return stack->top == stack->capacity - 1;
-}
- 
-// Stack is empty when top is equal to -1
-int isEmpty(struct Stack* stack)
-{
-    return stack->top == -1;
-}
- 
-// Function to add an item to stack.  It increases top by 1
-void push(struct Stack* stack, char item)
-{
-    if (isFull(stack))
-        return;
-    stack->array[++stack->top] = item;
-    printf("%d pushed to stack\n", item);
-}
- 
-// Function to remove an item from stack.  It decreases top by 1
-char pop(struct Stack* stack)
-{
-    if (isEmpty(stack))
-        return '\0';
-    return stack->array[stack->top--];
-}
- 
-// Function to return the top from stack without removing it
-char peek(struct Stack* stack)
-{
-    if (isEmpty(stack))
-        return '\0';
-    return stack->array[stack->top];
-}
 
 struct tnode {
 	char chr;
 	struct tnode *left;
 	struct tnode *right;
 };
-
-
-// A structure to represent a stack
-struct StackNode {
-    int top;
-    unsigned capacity;
-    char* array;
-};
- 
-// function to create a stack of given capacity. It initializes size of
-// stack as 0
-struct StackNode* createStackNode(unsigned capacity)
-{
-    struct StackNode* stack = (struct StackNode*)malloc(sizeof(struct StackNode));
-    stack->capacity = capacity;
-    stack->top = -1;
-    stack->array = (struct tnode*)malloc(stack->capacity * sizeof(struct tnode));
-    return stack;
-}
- 
-// Stack is full when top is equal to the last index
-int isFullStackNode(struct StackNode* stack)
-{
-    return stack->top == stack->capacity - 1;
-}
- 
-// Stack is empty when top is equal to -1
-int isEmptyStackNode(struct StackNode* stack)
-{
-    return stack->top == -1;
-}
- 
-// Function to add an item to stack.  It increases top by 1
-void pushStackNode(struct StackNode* stack, char item)
-{
-    if (isFullStackNode(stack))
-        return;
-    stack->array[++stack->top] = item;
-    printf("%d pushed to stack\n", item);
-}
- 
-// Function to remove an item from stack.  It decreases top by 1
-struct tnode *popStackNode(struct StackNode* stack)
-{
-    if (isEmptyStackNode(stack))
-        return NULL;
-    return stack->array[stack->top--];
-}
- 
-// Function to return the top from stack without removing it
-struct tnode *peekStackNode(struct StackNode* stack)
-{
-    if (isEmpty(stack))
-        return NULL;
-    return stack->array[stack->top];
-}
-
 
 char s[MAXN];
 int p = 0;
